53f45b003ca3cd927debeb3bde7cb65389d61899
Alex <stathis.alexander@gmail.com>
2022-01-22 03:03:21 -0500

    setup for idea generator

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Route, Routes} from "react-router";
+import NameGenerator from "./NameGenerator";
 
 
 const Router = () => {
     return (
         <Routes>
             <Route path="/" exact/>
-            <Route path="/idea-generator"/>
+            <Route path="/name-generator" element={<NameGenerator/>}/>
             <Route path="/code-analyzer"/>
         </Routes>
     )
