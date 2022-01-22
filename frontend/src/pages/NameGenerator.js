12d0b28f80434f78fdfe474f458e27cb6e35709e
Alex <stathis.alexander@gmail.com>
2022-01-22 17:59:37 -0500

    idea gen

--- frontend/src/pages/NameGenerator.js
+++ frontend/src/pages/NameGenerator.js
@@ -3,6 +3,8 @@ import {Container, FlexBox} from "../components";
 import styled from 'styled-components'
 import {HiOutlineChevronRight} from 'react-icons/hi'
 import API from "../api/api";
+import Typewriter from 'typewriter-effect';
+import {motion} from "framer-motion";
 
 
 const NameGenerator = () => {
@@ -11,20 +13,20 @@ const NameGenerator = () => {
     const [payload, setPayload] = useState({})
     const [messages, setMessages] = useState([
         {
-            sender: "Robot",
+            sender: "bot",
             message: "This is your name generating assistant."
         },
         {
-            sender: "Robot",
-            message: "Enter keywords separated by a space that have to do with your idea."
+            sender: "bot",
+            message: "Enter a description of your project and we'll give you a name."
         }
     ])
 
     useEffect(() => {
         if (receiveValue === 'description') {
             const message = {
-                sender: 'Robot',
-                message: 'Next add a description to your idea...'
+                sender: 'bot',
+                message: 'You can keep sending descriptions and we\'ll give you names'
             }
             setMessages([
                 ...messages, message
@@ -33,7 +35,7 @@ const NameGenerator = () => {
     }, [receiveValue])
 
     useEffect(() => {
-        if(!!payload.keywords && !!payload.description) {
+        if (!!payload.description) {
             findName()
         }
     }, [payload])
@@ -42,18 +44,14 @@ const NameGenerator = () => {
     const handleKeypress = (e) => {
         //it triggers by pressing the enter key
         if (e.keyCode === 13) {
-            if (receiveValue === 'keywords') {
-                submitKeywords()
-            }
-            if (receiveValue === 'description') {
-                submitDescription()
-            }
+            submitDescription()
+
         }
     };
 
     const submitDescription = () => {
         const message = {
-            sender: 'User@McHacks9',
+            sender: 'user@McHacks9',
             message: input
         }
         setMessages([
@@ -64,14 +62,13 @@ const NameGenerator = () => {
             description: input
         })
         setInput('')
-        setReceiveValue('keywords')
     }
 
     const findName = () => {
         API.post(`/test`, payload).then((res) => {
             console.log(res)
             const message = {
-                sender: 'Robot',
+                sender: 'bot',
                 message: res.data.name
             }
             setMessages([
@@ -81,30 +78,21 @@ const NameGenerator = () => {
         })
     }
 
-    const submitKeywords = () => {
-        const message = {
-            sender: 'User@McHacks/namegenerator',
-            message: input
-        }
-        setMessages([
-            ...messages, message
-        ])
-        setPayload({
-            ...payload,
-            keywords: input.split(' ')
-        })
-        setInput('')
-        setReceiveValue('description')
-    }
-
 
     return (
-        <Container direction={'column'}>
+        <Container
+            initial={{opacity: 0.8}}
+            animate={{opacity: 1}}
+            transition={{duration: 0.5}}
+            direction={'column'}>
             <div style={{maxHeight: '70vh', height: '70vh', overflowY: 'scroll', width: '100%'}}>
                 <Messages id={'messages'} fluid={"true"} direction={'column'} justify={'flex-end'} align={'flex-start'}
                           style={{paddingBottom: 20}}>
                     {messages.map((idea, id) =>
-                        <FlexBox key={id}>
+                        <FlexBox
+                            initial={{opacity: 0}}
+                            animate={{opacity: 1}}
+                                 key={id}>
                             <Text>{idea.sender} ></Text>
                             <Text>{idea.message}</Text>
                         </FlexBox>
@@ -150,7 +138,7 @@ const InputContainer = styled(FlexBox)`
   height: 10vh;
 `
 
-const Text = styled.p`
+const Text = styled(motion.p)`
   margin-left: 16px;
   font-size: 18px;
 `
