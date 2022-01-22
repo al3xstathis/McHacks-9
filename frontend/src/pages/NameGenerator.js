48804e7f4f54b649e7d535e1c2e08b5fd12072b6
Alex <stathis.alexander@gmail.com>
2022-01-22 16:10:34 -0500

    container

--- frontend/src/pages/NameGenerator.js
+++ frontend/src/pages/NameGenerator.js
@@ -21,7 +21,6 @@ const NameGenerator = () => {
     ])
 
     useEffect(() => {
-        console.log(payload)
         if (receiveValue === 'description') {
             const message = {
                 sender: 'Robot',
@@ -69,7 +68,7 @@ const NameGenerator = () => {
     }
 
     const findName = () => {
-        API.post(`/nameGenerator`, payload).then((res) => {
+        API.post(`/test`, payload).then((res) => {
             console.log(res)
             const message = {
                 sender: 'Robot',
@@ -100,41 +99,66 @@ const NameGenerator = () => {
 
 
     return (
-        <Container direction={'column'} >
-            <FlexBox fluid={"true"} direction={'column'} justify={'flex-end'} align={'flex-start'} style={{paddingBottom: 20}}>
-                {messages.map((idea, id) =>
-                    <FlexBox key={id}>
-                        <Text>{idea.sender} ></Text>
-                        <Text>{idea.message}</Text>
-                    </FlexBox>
-                )}
-            </FlexBox>
-            <FlexBox align={'center'} justify={'flex-start'} style={{paddingLeft: 20, width: '100%'}}>
-                <HiOutlineChevronRight style={{color: 'black'}}/>
-                <Input onKeyDown={(e) => handleKeypress(e)} value={input} onChange={e => setInput(e.target.value)}
-                        variant="unstyled" placeholder={"Add an Idea"}/>
-            </FlexBox>
+        <Container direction={'column'}>
+            <div style={{maxHeight: '70vh', height: '70vh', overflowY: 'scroll', width: '100%'}}>
+                <Messages id={'messages'} fluid={"true"} direction={'column'} justify={'flex-end'} align={'flex-start'}
+                          style={{paddingBottom: 20}}>
+                    {messages.map((idea, id) =>
+                        <FlexBox key={id}>
+                            <Text>{idea.sender} ></Text>
+                            <Text>{idea.message}</Text>
+                        </FlexBox>
+                    )}
+                </Messages>
+            </div>
+            <InputContainer align={'center'} justify={'flex-start'}>
+                <HiOutlineChevronRight style={{color: 'white', width: '40px', fontSize: 30, paddingLeft: 20}}/>
+                <Input onKeyDown={(e) => {
+                    handleKeypress(e)
+                }} value={input} onChange={e => setInput(e.target.value)}
+                       variant="unstyled" placeholder={"Add an Idea"}/>
+            </InputContainer>
         </Container>
     )
 }
 
 export default NameGenerator
 
+
+const Messages = styled(FlexBox)`
+`
+
 const Input = styled.input`
   font-size: 20px;
   background-color: transparent;
   border: none;
-  color: black;
+  padding: 20px;
+  width: 100%;
   &:focus {
     outline: none;
   }
+  &::placeholder {
+  color: white;
+  opacity: 0.8;
+  }
+`
+
+const InputContainer = styled(FlexBox)`
+  width: 100%;
+  box-shadow:0 0 0 1px black inset;
+  margin-inside: 20px;
+  height: 10vh;
 `
 
 const Text = styled.p`
-color: black;
   margin-left: 16px;
+  font-size: 18px;
 `
 
 const Container = styled(FlexBox)`
-  height: 80%;
+  background-color: grey;
+  box-shadow:0 0 0 1px black inset;
+  color: white;
+  height: 80vh;
+overflow: hidden;
 `
