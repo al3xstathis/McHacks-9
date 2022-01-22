d422545e0081d87c74427d2ec5e583e72818845f
Dina Khalesi <dinakhalesit@gmail.com>
2022-01-22 15:58:45 -0500

    created components for routes

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Route, Routes } from "react-router";
+import { CodeAnalyzer } from './CodeAnalyzer';
 import { IdeaGenerator } from './IdeaGenerator';
 
 
 const Router = () => {
     return (
         <Routes>
-            <Route path="/" exact />
+            <Route path="/" exact element={<Home />} />
             <Route path="/idea-generator" element={<IdeaGenerator />} />
             <Route path="/name-generator" />
-            <Route path="/code-analyzer" />
+            <Route path="/code-analyzer" element={<CodeAnalyzer />} />
         </Routes>
     )
 }
